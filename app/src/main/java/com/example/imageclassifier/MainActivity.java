acdb69eb92eb7c7ee0b2b7afe7900a782b96b8de
ayoub mesquiny <ayoubm121@gmail.com>
2023-01-22 23:03:47 +0100

    first commit

---
+++ app/src/main/java/com/example/imageclassifier/MainActivity.java
@@ -0,0 +1,163 @@
+package com.example.imageclassifier;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.TextView;
+
+import com.example.imageclassifier.ml.MobilenetV110224Quant;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button selectBtn,captureBtn,predictBtn;
+    Bitmap bitmap;
+    ImageView image;
+    TextView result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getPermission();
+
+        String[] labels = new String[1001];
+        int cnt = 0;
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(getAssets().open("labels.txt")));
+            String line = bufferedReader.readLine();
+            while (line!=null){
+                labels[cnt]=line;
+                cnt++;
+                line=bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        selectBtn = findViewById(R.id.select_image_btn);
+        captureBtn = findViewById(R.id.capture_image_btn);
+        predictBtn= findViewById(R.id.prediction_btn);
+        image = findViewById(R.id.image);
+        result = findViewById(R.id.result);
+
+        selectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent,10);
+            }
+        });
+
+        captureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,11);
+
+
+            }
+        });
+
+        predictBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    MobilenetV110224Quant model = MobilenetV110224Quant.newInstance(MainActivity.this);
+
+                    // Creates inputs for reference.
+                    TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.UINT8);
+
+                    bitmap = Bitmap.createScaledBitmap(bitmap,224,224,true);
+                    inputFeature0.loadBuffer(TensorImage.fromBitmap(bitmap).getBuffer());
+
+                    // Runs model inference and gets result.
+                    MobilenetV110224Quant.Outputs outputs = model.process(inputFeature0);
+                    TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+                    result.setText(labels[getMax(outputFeature0.getFloatArray())]+ " ");
+
+                    // Releases model resources if no longer used.
+                    model.close();
+                } catch (IOException e) {
+                }
+            }
+        });
+    }
+
+    int getMax(float[] arr){
+        int max = 0;
+        for (int i=0;i<arr.length;i++){
+            if (arr[i]>arr[max]){
+                max=i;
+            }
+        }
+
+        return max;
+
+    }
+
+    void getPermission(){
+        if (checkSelfPermission(android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { android.Manifest.permission.CAMERA }, 11);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode==11){
+            if (grantResults.length>0){
+                if (grantResults[0]!=PackageManager.PERMISSION_GRANTED){
+                    this.getPermission();
+                }
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode==10){
+            if (data!=null){
+                Uri uri = data.getData();
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),uri);
+                    image.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        if (requestCode==11){
+            bitmap = (Bitmap) data.getExtras().get("data");
+            image.setImageBitmap(bitmap);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
